356e88428eb260185e343c0b5897c49819565310
Hector Mejia <mejia7hector@gmail.com>
2021-12-08 12:12:59 -0600

    adds hello-asteroid

---
+++ hello-asteroid.js/hello-asteroid.js
@@ -0,0 +1,9 @@
+
+
+//console.log("Actually," + " greek yogurt is one of my favorite foods");
+var favoriteFood = "Greek yogurt"
+console.log("My favorite food is  " + favoriteFood+ "!")
+
+favoriteFood = "Double Quarter Pounder from Mickey D's";
+var phraseOne = "Ok, change of plans. I am craving \n";
+    console.log(phraseOne  + favoriteFood)
