826128e069a41076e5b152421acdcf6de506d97a
Hector Mejia <mejia7hector@gmail.com>
2021-12-10 21:44:20 -0600

    javascript-html-exercises

--- js/external.js
+++ js/external.js
@@ -2,8 +2,13 @@ console.log('Hello from JavaScript!');
 
 alert("Welcome to my Website!");
 
-var userInput = prompt('What is your favorite color:');
+let userInput = prompt('What is your favorite color:');
 console.log('The user entered: ' + userInput);
 if (userInput.toLowerCase() == userInput) {
     alert("Great, " + userInput + " is my favorite color too!" );
 }
+let selectedTime = prompt("To enroll in the class, what time are you available? Please type a time if any " + "time between 10am, 11am, 12pm.");
+if (selectedTime >= 12 || selectedTime < 10) {
+    alert("sorry please choose an earlier time")
+}   else
+        alert("Congrats")
