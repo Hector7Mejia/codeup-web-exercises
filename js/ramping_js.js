d6dd94fd2d1f2e717800b170967b672a56a47876
Hector Mejia <mejia7hector@gmail.com>
2022-02-03 13:47:11 -0600

    query_exercise

---
+++ js/ramping_js.js
@@ -0,0 +1,34 @@
+// Write a function called findAverage that takes in an array of integers representing grades and returns the average.
+//     Example input: [95, 74, 86, 100]
+// Example output: 88.75
+
+// const average = (array) => array.reduce((a, b) => a + b) / array.length;
+// console.log(average([1,500]));
+
+// write a function called sumOfSquares(a, b)
+// takes 2 parameters called a and b
+// returns the sum of the squares of a and b
+// i.e., (a to the power of 2) + (b to the power of 2)
+// e.g., console.log(sumOfSquares(2, 3));
+// will return 13.
+
+// function sumOfSquares(a,b) {
+//     return (a * a) + (b * b)
+// }
+//
+// console.log(sumOfSquares(5,5))
+
+// Write a function called whichStringIsLonger() that takes two parameters and returns one of 4 values:
+//     returns "first" if the first string parameter is longer
+// returns "second" if the second parameter is longer
+// returns "neither" if they are the same length
+// returns false if either or both of the parameters are not strings
+
+function whichStringIsLonger(word1, word2) {
+    if(word1.length > word2.length) {
+        return "first";
+    }
+    if(word1.length < word2.length) {
+        return "second";
+    }
+}
--- js/warm-ups.js
+++ js/warm-ups.js
@@ -197,3 +197,38 @@ function addNums(num1, num2) {
 }
 
 console.log(addNums(5,7))*/
+
+//     function reverseString(str) {
+//         return str;
+//     }
+//     reverseString("hello");
+// }
+
+"use strict";
+
+var myNameIs = 'Hector'; // TODO: Fill in your name here.
+
+// TODO:
+// Create a function called 'sayHello' that takes a parameter 'name'.
+
+function sayHello(name) {
+
+}
+// // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+// sayHello(myNameIs);
+// // Don't modify the following line
+// // It generates a random number between 1 and 100 and stores it in random
+// var random = Math.floor((Math.random()*100)+1);
+//
+// // TODO:
+// // Create a function called 'isOdd' that takes a number as a parameter.
+// // The function should use the ternary operator to log a message.
+// // The log should tell the number passed in and whether it is odd or not.
+// let result = ""
+// function isOdd(num) {
+//     let result = (num % 2 === 0) ? console.log("your number is even") : console.log("your number is odd");
+// }
+//
+// // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+//
+// isOdd(random);
