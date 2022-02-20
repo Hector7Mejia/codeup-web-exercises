a94891d7dba3178f0b1eccd0668e1af9f8f22d24
Hector Mejia <mejia7hector@gmail.com>
2022-02-19 21:34:08 -0600

    finished_dom_query.js

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,15 @@
-const githubPromise = fetch('https://api.github.com/users', {headers: {Authorization: 'token ${GIT_API}'}} )
-    .then(response => response.json())
-    .catch(error => console.error(error));
-console.log(githubPromise)
+const baseURL = 'https://api.github.com';
+
+function getUserLastPushEvent(username) {
+    return fetch(baseURL + '/users/' + username + '/events/public', {headers: {'Authorization': 'token ${GIT_API}'}})
+        .then(res => res.json())
+        .then(events => {
+            for (let event of events) {
+                if (event.type === 'PushEvent') {
+
+                }
+            }
+        });
+}
+
+getUserLastPushEvent('topfunky').then(lastEventDate => console.log(lastEventDate));
