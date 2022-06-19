5773f0426acee064de173977670369fe528da6a9
Hector Mejia <mejia7hector@gmail.com>
2022-06-19 13:10:10 -0500

    .reduce practice

--- js/practicing.js
+++ js/practicing.js
@@ -33,3 +33,41 @@
 // }
 //
 // console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
+const person = { name: "Tim"};
+const student = { ID: "21", GPA: "3.0"};
+// const students = { ...person, ...student1, ...student, semester: '4'};
+let calculatedGPA = [];
+const student1 = {name: "One", ID: "1", GPA: 2.0};
+const student2 = {name: "Two", ID: "2", GPA: 4.0};
+const student3 = {name: "Three", ID: "3", GPA: 4.0};
+const student4 = {name: "Four", ID: "4", GPA: 2.4};
+let testArray = [student1, student2, student3, student4];
+// function myFunc(total, num) {
+//     return total + num;
+// }
+// function calculateGPA(students) {
+//
+//     for (let i = 0; i < students.length; i++) {
+//
+//         calculatedGPA += students[i].GPA
+//     }
+//     console.log(calculatedGPA.reduce(myFunc));
+// }
+//
+// console.log(calculateGPA(testArray));
+//
+// const arr = [
+//     {id: 1, salary: 10},
+//     {id: 2, salary: 20},
+//     {id: 3, salary: 30},
+// ];
+//
+// .reduce is using accumulator and object to specify what is happening to its paramater
+// function addGPA(testArray) {
+//     const sum = testArray.reduce((accumulator, object) => {
+//         return accumulator + object.GPA;
+//     }, 0);
+//     console.log(sum/testArray.length);
+// }
+// addGPA(testArray);
