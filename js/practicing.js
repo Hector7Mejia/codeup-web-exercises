6aca7074ec9c6f18b3d579ef4b15b68f56eb1d6d
Hector Mejia <mejia7hector@gmail.com>
2022-07-06 18:40:39 -0500

    art-like js

--- js/practicing.js
+++ js/practicing.js
@@ -95,3 +95,25 @@ function calculateGPA(testArray1) {
 //
 // bananaGreeting("georgie");
 
+//function processData(input) {
+//     let inputCopy = input.split('\n').slice(1);
+//     inputCopy.forEach((str) => {
+//       let evenStr = '';
+//       let oddStr = '';
+//         for(let i = 0; i < str.length; i++) {
+//             i % 2 ? oddStr += str[i] : evenStr += str[i];
+//         }
+//         console.log(`${evenStr} ${oddStr}`);
+//     });
+// }
+//
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// _input = "";
+// process.stdin.on("data", function (input) {
+//     _input += input;
+// });
+//
+// process.stdin.on("end", function () {
+//    processData(_input);
+// });
