0ac287c1950b499c3c56841574f6721a4c12a4cd
Hector Mejia <mejia7hector@gmail.com>
2022-07-23 16:36:17 -0500

    arrows

---
+++ js/arrowfuncs.js
@@ -0,0 +1,9 @@
+// const conPractice = name => console.log(name.indexOf(name));
+
+let arrayArrow = [1,2,3,4,5,6,7,8,9];
+
+// const conPractice =
+
+// conPractice(arrayArrow);
+
+// console.log(arrayArrow.map(name => name + name))
