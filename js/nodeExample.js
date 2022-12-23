a6b79b4194054b5dfb63349fe93e0335aa53a862
Hector Mejia <mejia7hector@gmail.com>
2022-12-22 20:39:31 -0500

    linkedListNode

---
+++ js/nodeExample.js
@@ -0,0 +1,13 @@
+class LinkedListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+}
